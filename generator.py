b8a057ac2192f3290da6f93ea7d09e07639e3a36
samy440 <samy.coulombe@mail.mcgill.ca>
2018-01-25 14:20:00 -0500

    both MHP sim outcome and door frequency plots are now plotting in real-time

--- generator.py
+++ generator.py
@@ -15,9 +15,19 @@
 from random import randint
 import matplotlib.pyplot as plt
 from matplotlib import style
+import matplotlib
+matplotlib.use('TKAgg')
+import numpy as np
 style.use('fivethirtyeight')
 
 def generator(n, createFile):
+    plt.ion()
+    fig = plt.figure()
+    figA = plt.subplot2grid((1,7), (0,0), rowspan=1, colspan=4)
+    figB = plt.subplot2grid((1,7), (0,5), rowspan=1, colspan=2)
+    plt.ylim([0.0, 1.0])
+    x_axis_bar_graph = ("Door #1", "Door #2", "Door #3")
+    x_axis_bar_graph_values = [1,2,3]
 
     car_location_list = [ randint(0,2) for i in range(n) ]
     initial_guess_list = [ randint(0,2) for i in range(n) ]
@@ -29,11 +39,19 @@ def generator(n, createFile):
     switched_outcome_tally = 0.0
     switched_outcome_tally_list = [ ]
     car_door_distribution = [0,0,0]
+    car_door_distribution_at_iter = np.array(car_door_distribution)
+        
+    rects = figB.bar(x_axis_bar_graph_values, car_door_distribution_at_iter, align='center', alpha=0.5)
 
     for iter in range(0,n):
         total_iterations_so_far = (iter + 1)
 
-        car_door_distribution[car_location_list[iter]] += 1
+        car_door_distribution[car_location_list[iter]] += (1.0/n)
+        car_door_distribution_at_iter = np.array(car_door_distribution)
+        for rect, h in zip(rects, car_door_distribution_at_iter):
+            rect.set_height(h)
+        fig.canvas.draw()
+
 
         revealable_doors = [ x for x in [0,1,2] if ((x != car_location_list[iter]) and (x != initial_guess_list[iter])) ]
         monty_reveals = randint(0,(len(revealable_doors)-1))
@@ -56,7 +74,9 @@ def generator(n, createFile):
             stayed_outcome_tally *= iter
             stayed_outcome_tally /= total_iterations_so_far
             stayed_outcome_tally_list.append(stayed_outcome_tally)
-    
+        figA.scatter(total_iterations_so_far, stayed_outcome_tally_list[iter], color='red')
+        figA.scatter(total_iterations_so_far, switched_outcome_tally_list[iter], color='blue')
+        plt.pause(0.01)
     if createFile:
         MHPSim_record = open("MHPSim_record.csv","w")
         MHPSim_record.write("[Simulation Number],[Car Location (1/2/3)],[Initial Guess (1/2/3)],[Switch Guess (1/2/3)],[Stayed Outcome (1/0)],[Switched Outcome (1/0)]\n")
@@ -64,25 +84,5 @@ def generator(n, createFile):
             MHPSim_record.write(str(x+1) + "," + str((car_location_list[x])+1) + "," + str((initial_guess_list[x])+1) + "," + str((switched_guess_list[x][0])+1) + "," + str(stayed_outcome[x]+1) + "," + str(switched_outcome[x]+1) + "\n")
         MHPSim_record.close()
 
-
-    display(n, car_door_distribution, stayed_outcome_tally_list, switched_outcome_tally_list)
-
-def display(n, door_frequencies, stayed_outcome_list, switched_outcome_list):
-    for df in range(0,len(door_frequencies)):
-        door_frequencies[df] /= n
-    
-    fig = plt.figure()
-    figA = plt.subplot2grid((1,7), (0,0), rowspan=1, colspan=4)
-    figB = plt.subplot2grid((1,7), (0,5), rowspan=1, colspan=2)
-
-    figA.scatter([x for x in range(n)], stayed_outcome_list)
-    figA.scatter([x for x in range(n)], switched_outcome_list)
-
-    x_axis_bar_graph = ("Door #1", "Door #2", "Door #3")
-    x_axis_bar_graph_values = [0,1,2]
-    figB.bar(x_axis_bar_graph_values, door_frequencies, align='center', alpha=0.5)
-    
-    plt.show()
-    
 if __name__ == '__main__':
     generator()
