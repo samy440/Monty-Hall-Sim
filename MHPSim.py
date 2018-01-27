7002104d7fc32ef6eba1bb73e924face078330cf
samy440 <samy.coulombe@mail.mcgill.ca>
2018-01-26 21:26:23 -0500

    fixed user input validation

---
+++ MHPSim.py
@@ -0,0 +1,30 @@
+import generator
+
+def main():
+    print("""Welcome to this Monty Hall Problem Simulator!\n
+    This program aims to run simulations of the Monty Hall Problem
+     with the stay/switch strategies\nand plot their outcome in 
+     real time.\n""")
+    input_number_of_iterations = input("Please input the number of simulations to run:  ")
+    valid_input = intInput(input_number_of_iterations)
+    while (valid_input < 0):
+        print("The input value for the number of simulations to run must be an integer.")
+        print("You have entered a " + str(type(input_number_of_iterations)) + ".\n")
+        input_number_of_iterations = input("Please input the number of simulations to run:  ")
+        valid_input = intInput(input_number_of_iterations)
+    generator.generator(valid_input, True)
+    print("Done")
+
+def intInput(inp):
+    try:
+        int_inp = int(inp)
+    except ValueError:
+        try:
+            float_inp = float(inp)
+        except ValueError:
+            return (-1)
+        return int(float_inp)
+    return int_inp
+
+if __name__ == '__main__':
+    main()
--- generator.py
+++ generator.py
@@ -1,96 +1,83 @@
-# Description:
-# This script generates two temporary data files corresponding to
-# the switch/stay strategies. These files (MPHSim_temp_stay/switch.csv)
-# contain the outcome, final door pick, and car door for each iteration
-# according to the following format and type:
-# [outcome = boolean], [final door pick = int {0,1,2}], [car door = int {0,1,2}].
-# 
-# generator.py is meant to be called by the main.py script. 
-#
-# Future work:  add a stats-compiling function and output file,
-#               allow for a random strategy (randomly choose to switch or stay)
-#               add a parameter representing the number of doors such that there
-#               are >= 3 doors to choose from (but still only one car).
-
+# import block
 from random import randint
-import matplotlib.pyplot as plt
-from matplotlib import style
 import matplotlib
 matplotlib.use('TKAgg')
+import matplotlib.pyplot as plt
+from matplotlib import style
 import numpy as np
 style.use('fivethirtyeight')
 
 def generator(n, createFile):
-    plt.ion()
-    fig = plt.figure()
-    mng = plt.get_current_fig_manager()
-    mng.resize(*mng.window.maxsize())
-    figA = plt.subplot2grid((1,7), (0,0), rowspan=1, colspan=4)
+    # Called from the main() function in MHPSim.py
+    plt.ion()   # enables interactive mode for plots
+    fig = plt.figure()  # initializes the overall plot
+    mng = plt.get_current_fig_manager() # used to make the figure apppear in full-screen mode
+    mng.resize(*mng.window.maxsize())   # also used to make the figure apppear in full-screen mode
+    figA = plt.subplot2grid((1, 7), (0, 0), rowspan=1, colspan=4) # figA is the simulation outcome plot
     figA.set_title("Outcome of Simulations\n")
     figA.set_xlabel("Number of Simulations")
-    figA.set_ylabel("Rate of Success\n(Ending the Simulation with a Car)")
+    figA.set_ylabel("Cummulative  Success Rate (%)\n(Ending the Simulation with a Car)")
 
-    figB = plt.subplot2grid((1,7), (0,5), rowspan=1, colspan=2)
+    figB = plt.subplot2grid((1, 7), (0, 5), rowspan=1, colspan=2)   # figB is the door distribution plot
     figB.set_title("Distribution of Car Location\n")
     figB.set_xlabel("Door Number")
-    figB.set_ylabel("Frequency of Holding Car")
-    plt.ylim([0.0, 1.0])
-    x_axis_bar_graph = ("Door #1", "Door #2", "Door #3")
-    x_axis_bar_graph_values = [1,2,3]
+    figB.set_ylabel("Frequency of Car Location Over " + str(n) + " Simulations (%)")
+    plt.ylim([0.0, 100.0])
+    x_axis_bar_graph_values = [1, 2, 3]
 
-    car_location_list = [ randint(0,2) for i in range(n) ]
-    initial_guess_list = [ randint(0,2) for i in range(n) ]
-    switched_guess_list = [ ]
-    switched_outcome = [ 0 for i in range(n) ]
-    stayed_outcome = [ 0 for i in range(n) ]
+    car_location_list = [ randint(0, 2) for i in range(n) ] # a list of n integers randomly chosen between [0, 1, 2] representing which door has the car
+    initial_guess_list = [ randint(0, 2) for i in range(n) ]    # a list of n integers randomly chosen between [0, 1, 2] representing the contestant's initial door-pick
+    switched_guess_list = [ ]   # a list which will be populated to contain the n integers representing the contestant's switched door-pick
+    switched_outcome = [ 0 for i in range(n) ]  # a list which will be updated to contain the outcome (car/no car == 1/0) of switching the door-pick in the current simulation
+    stayed_outcome = [ 0 for i in range(n) ]    # a list which will be updated to contain the outcome (car/no car == 1/0) of staying with the original door-pick in the current simulation
     stayed_outcome_tally = 0.0
     stayed_outcome_tally_list = [ ]
     switched_outcome_tally = 0.0
     switched_outcome_tally_list = [ ]
-    car_door_distribution = [0,0,0]
-    car_door_distribution_at_iter = np.array(car_door_distribution)
+    car_door_distribution = [0, 0, 0]   # a list representing the distribution of the supposedly random selection of door containing the car
+    car_door_distribution_at_sim = np.array(car_door_distribution)
         
-    rects = figB.bar(x_axis_bar_graph_values, car_door_distribution_at_iter, align='center', alpha=0.5)
+    rects = figB.bar(x_axis_bar_graph_values, car_door_distribution_at_sim, align='center', alpha=0.5)
 
-    for iter in range(0,n):
-        total_iterations_so_far = (iter + 1)
+    for sim in range(0, n):
+        total_iterations_so_far = (sim+ 1) 
 
-        car_door_distribution[car_location_list[iter]] += (1.0/n)
-        car_door_distribution_at_iter = np.array(car_door_distribution)
-        for rect, h in zip(rects, car_door_distribution_at_iter):
+        car_door_distribution[car_location_list[sim]] += (1.0/n)*100
+        car_door_distribution_at_sim = np.array(car_door_distribution)
+        for rect, h in zip(rects, car_door_distribution_at_sim):
             rect.set_height(h)
         fig.canvas.draw()
 
-        revealable_doors = [ x for x in [0,1,2] if ((x != car_location_list[iter]) and (x != initial_guess_list[iter])) ]
+        revealable_doors = [ x for x in [0,1,2] if ((x != car_location_list[sim]) and (x != initial_guess_list[sim])) ]
         monty_reveals = randint(0,(len(revealable_doors)-1))
-        switched_guess_list.append( [ y for y in [0,1,2] if ((y != revealable_doors[monty_reveals]) and (y != initial_guess_list[iter]))] )
-        if initial_guess_list[iter] == car_location_list[iter]:
-            stayed_outcome[iter] = 1
-            stayed_outcome_tally *= iter
+        switched_guess_list.append( [ y for y in [0,1,2] if ((y != revealable_doors[monty_reveals]) and (y != initial_guess_list[sim]))] )
+        if initial_guess_list[sim] == car_location_list[sim]:
+            stayed_outcome[sim] = 1
+            stayed_outcome_tally *= sim
             stayed_outcome_tally += 1.0
             stayed_outcome_tally /= total_iterations_so_far
             stayed_outcome_tally_list.append(stayed_outcome_tally)
-            switched_outcome_tally *= iter
+            switched_outcome_tally *= sim
             switched_outcome_tally /= total_iterations_so_far
             switched_outcome_tally_list.append(switched_outcome_tally)
-        if switched_guess_list[iter][0] == car_location_list[iter]:
-            switched_outcome[iter] = 1
-            switched_outcome_tally *= iter
+        if switched_guess_list[sim][0] == car_location_list[sim]:
+            switched_outcome[sim] = 1
+            switched_outcome_tally *= sim
             switched_outcome_tally += 1.0
             switched_outcome_tally /= total_iterations_so_far
             switched_outcome_tally_list.append(switched_outcome_tally)
-            stayed_outcome_tally *= iter
+            stayed_outcome_tally *= sim
             stayed_outcome_tally /= total_iterations_so_far
             stayed_outcome_tally_list.append(stayed_outcome_tally)
-        figA.scatter(total_iterations_so_far, stayed_outcome_tally_list[iter], color='red')
-        figA.scatter(total_iterations_so_far, switched_outcome_tally_list[iter], color='blue')
-        
-
+        figA.scatter(total_iterations_so_far, stayed_outcome_tally_list[sim]*100, color='red')
+        figA.scatter(total_iterations_so_far, switched_outcome_tally_list[sim]*100, color='blue')
         plt.pause(0.005)
-    figA.plot([ x for x in range(1, (n+1))], stayed_outcome_tally_list, color='red', label='Always Stay')
-    figA.plot([ x for x in range(1, (n+1))], switched_outcome_tally_list, color='blue', label='Always Switch')
+
+    figA.plot([ x for x in range(1, (n+1))], [ (y*100) for y in stayed_outcome_tally_list ], color='red', label='Always Stay')
+    figA.plot([ x for x in range(1, (n+1))], [ (y*100) for y in switched_outcome_tally_list ], color='blue', label='Always Switch')
     figA.legend(loc='best')
-    plt.pause(10)
+    plt.pause(60)
+
     if createFile:
         MHPSim_record = open("MHPSim_record.csv","w")
         MHPSim_record.write("[Simulation Number],[Car Location (1/2/3)],[Initial Guess (1/2/3)],[Switch Guess (1/2/3)],[Stayed Outcome (1/0)],[Switched Outcome (1/0)]\n")
